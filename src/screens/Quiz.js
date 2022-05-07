aa2b18b668e1023308191c88ae437749463372f9
Andres Obregon <andresnicolasobregon@gmail.com>
2022-05-07 18:39:08 -0300

    first commit

---
+++ src/screens/Quiz.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+import {
+  Text,
+  View,
+} from 'react-native';
+import { COLORS, SIZES } from '../constants';
+import data from '../data/QuizData';
+
+
+const Quiz = () => {
+  
+  return (
+    <View>
+      <Text>Hola</Text>
+    </View>
+    
+  );
+};
+
+
+export default Quiz;
